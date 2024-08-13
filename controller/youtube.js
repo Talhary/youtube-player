fcb92d97805968499867d2a660d6299fcf224a3d
Muhammad Talha <talhariaz5425869@gmail.com>
2024-08-13 12:19:59 +0000

    updated

--- controller/youtube.js
+++ controller/youtube.js
@@ -2,29 +2,43 @@ const { alldown } = require("nayan-media-downloader");
 const fs = require('fs');
 const axios = require('axios');
 const path = require('path')
+let ParseDomain  = import('parse-domain')
 const getYtInfo = async (req, res) => {
+    ParseDomain = await ParseDomain
+   
     const { url,quality } = req.query;
     if (!url) {
         res.status(400).json({ status: 400, message: 'we need url mf' });
         return;
     }
-    
+    const domain = extractDomain(url)
     try {
         const data = await alldown(url);
         let dUrl;
-        // console.log(data)
+        let title;
+        console.log(domain)
+        if(domain == 'youtube.com' || domain == 'youtu.be'){
         if(quality=='low'){
-           dUrl = data.data.low
+           dUrl = data.data.low;
         }
         else{
-           dUrl = data.data.high
+           dUrl = data.data.high;
 
         }
          ;
-        const title = data.data.title;
-        const sanitizedTitle = title.slice(0,20).replace(/\s+/g, ''); // Remove spaces from the title
+         title =  data.data.title;
+        }
+        
+        else {
+            
+           return res.json({status:400,message:'Unsupported Domain'}).status(200);
+        }
+         
+        const sanitizedTitle = title.slice(0,30).replace(/\s+/g, ''); // Remove spaces from the title
         const filePath = `./downloads/${sanitizedTitle}.mp4`; // Set the path where the file will be saved
-
+        if(fs.existsSync(filePath)){
+            fs.unlinkSync(filePath)
+        }
         // Ensure the downloads directory exists
         if (!fs.existsSync('./downloads')) {
             fs.mkdirSync('./downloads');
@@ -42,6 +56,7 @@ const getYtInfo = async (req, res) => {
         // Ensure the download is complete before responding
         response.data.on('end', () => {
             const pathOffile = path.join(__dirname,'.'+filePath)
+            res.setHeader('Content-Disposition', 'attachment; filename=' + sanitizedTitle);
             res.sendFile(pathOffile)
             setTimeout(()=>{
                 if(fs.existsSync(pathOffile))
@@ -51,6 +66,7 @@ const getYtInfo = async (req, res) => {
 
         response.data.on('error', (err) => {
             console.error('Download failed:', err);
+            
             res.status(500).json({ status: 500, message: 'Error downloading file', error: err.message });
             
         });
@@ -61,3 +77,9 @@ const getYtInfo = async (req, res) => {
 };
 
 module.exports = { getYtInfo };
+
+function extractDomain(url) {
+    const domainPattern = /^(?:https?:\/\/)?(?:www\.)?([^\/]+)/i;
+    const match = url.match(domainPattern);
+    return match ? match[1] : null;
+}
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 //modules
 const fs = require('fs')
+const cors = require('cors')
+
+
+
 const youtubeRoute = require('./route/get-yt-info')
 
 //Express stuff and environment variables
@@ -11,8 +15,12 @@ require('express-async-errors')
 
 //Used By Express
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5500',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+   ))
 app.use(express.static('./public'))
-
 //routes
 app.use('/api/v1',youtubeRoute)
 
