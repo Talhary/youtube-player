da50734594c007aa1c61825394bf9b9f672f3cbe
Muhammad Talha <talhariaz5425869@gmail.com>
2024-08-16 06:24:46 +0000

    addedm3u8

---
+++ controller/m3u8.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const fs= require('fs');
+const m3u8stream = require('m3u8stream')
+const M3U8 = async (req, res) => {
+ try {
+    const url = req.query.url;
+
+    if (!url) {
+        return res.status(400).send('URL is required');
+    }
+
+    const fileName = `../downloads/video-${Date.now()}.ts`;
+    const filePath = path.join(__dirname, fileName);
+
+    const stream = m3u8stream(url);
+    const writeStream = fs.createWriteStream(filePath);
+
+    // Pipe the stream to the file
+    stream.pipe(writeStream);
+
+    // Listen for the finish event to know when the file is written
+    writeStream.on('finish', () => {
+        // Set headers for the file download
+        res.sendFile(filePath, (err) => {
+            if (err) {
+                console.error('Error sending file:', err);
+                res.status(500).send('Error sending file');
+            }
+            // Clean up the file after sending
+            fs.unlink(filePath, (unlinkErr) => {
+                if (unlinkErr) {
+                    console.error('Error deleting file:', unlinkErr);
+                }
+            });
+        });
+    });
+
+    // Handle errors in the streaming process
+    stream.on('error', (err) => {
+        console.error('Stream error:', err);
+        res.status(500).send('Error downloading video');
+    });
+
+    writeStream.on('error', (err) => {
+        console.error('Write stream error:', err);
+        res.status(500).send('Error saving video');
+    });
+ } catch (error) {
+    res.json({status:500,message:error.message}).status(500)
+ }
+}
+module.exports = {M3U8}
