03474fa8777a67fc00976af0f5feb44e49f549a7
Muhammad Talha <talhariaz5425869@gmail.com>
2024-08-18 12:13:37 +0000

    fsd

--- route/get-yt-info.js
+++ route/get-yt-info.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const route = express.Router()
 const {getYtInfo} = require('../controller/youtube')
-const {M3U8} = require('../controller/m3u8')
+
 route.route('/video').get(getYtInfo)
-route.route('/m3u8').get(M3U8)
+
 module.exports = route
